5c6199ce5aba803f4f4d738514d967823d5a8285
NftaliaK <63368900+NftaliaK@users.noreply.github.com>
2020-04-09 22:58:30 +0500

    commit1

--- app/src/main/java/com/example/verdo/MainActivity.java
+++ app/src/main/java/com/example/verdo/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         //hi
+        //hi
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
